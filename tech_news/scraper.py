a8cad41dbcef41011c4090eaa984ac65b95f57d3
phvleite <phvleite@gmail.com>
2022-11-10 09:59:01 -0300

    feat: implementado a função fetch(url) em scraper.py

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,20 @@
 # Requisito 1
+import requests
+import time
+
+
 def fetch(url):
     """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(
+            url, timeout=3, headers={"User-agent": "Fake user-agent"}
+        )
+        response.raise_for_status()
+    except requests.exceptions.RequestException:
+        return None
+    else:
+        return response.text
 
 
 # Requisito 2
@@ -21,3 +35,14 @@ def scrape_noticia(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
+# if __name__ == "__main__":
+#     for ind in range(1):
+#         print(ind)
+#         response1 = fetch("https://blog.betrybe.com/")
+#         print(f"1 - {response1}")
+#         response2 = fetch("http://httpbin.org/delay/2")
+#         print(f"2 - {response2}")
+#         response3 = fetch("http://httpbin.org/status/404")
+#         print(f"3 - {response3}")
