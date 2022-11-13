c96e02190e830c1648b2de518bd70e9dbe9560d4
phvleite <phvleite@gmail.com>
2022-11-12 21:19:55 -0300

    feat: implementada a captura da quantidade de comentários na função scrape_noticia()

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -38,11 +38,18 @@ def scrape_noticia(html_content):
     selector = Selector(text=html_content)
     url = selector.css("head link[rel*='canonical']::attr(href)").get()
     title = selector.css("h1.entry-title::text").get()
+    title = title.split(" ")
+    title = " ".join(title)
     timestamp = selector.css("li.meta-date::text").get()
     if len(timestamp) == 0:
         timestamp = selector.css("p.post-modified-info::text").get()[:10]
     writer = selector.css("a.url.fn.n::text").get()
-    print(url, title, timestamp, writer, sep=",")
+    comments_count = selector.css("h5.title-block::text").re(r"\d")
+    if len(comments_count) == 0:
+        comments_count = 0
+    else:
+        comments_count = int(comments_count[0])
+    print(url, title, timestamp, writer, comments_count, sep=",")
 
 
 # Requisito 5
@@ -62,20 +69,32 @@ if __name__ == "__main__":
     #     url = scrape_next_page_link(response)
     #     print(url)
     URL_BASE = "https://blog.betrybe.com/"
-    URL_EXTENSAO = "framework-de-programacao/frameworks-nodejs/"
+    URL_EXTENSAO = (
+        "tecnologia/informatica-basica/"
+    )
+    URL_EXT = ""
+    URL_EXTENSAO = URL_EXTENSAO + URL_EXT
     response = fetch(URL_BASE + URL_EXTENSAO)
     selector = Selector(text=response)
     url_current = selector.css("head link[rel*='canonical']::attr(href)").get()
     title = selector.css("h1.entry-title::text").get()
+    title = title.split(" ")
+    title = " ".join(title)
     timestamp = selector.css("li.meta-date::text").get()
     if len(timestamp) == 0:
         timestamp = selector.css("p.post-modified-info::text").get()[:10]
     writer = selector.css("a.url.fn.n::text").get()
+    comments_count = selector.css("h5.title-block::text").re(r"\d")
+    if len(comments_count) == 0:
+        comments_count = 0
+    else:
+        comments_count = int(comments_count[0])
     print(
         f"""
 url - {url_current}
 título - {title}
 data - {timestamp}
 escritor - {writer}
+comentários - {comments_count}
 """
     )
