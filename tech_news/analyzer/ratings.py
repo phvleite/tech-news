90a3dc31518a00d3596a38453ff5cad98746b9d4
phvleite <phvleite@gmail.com>
2022-11-15 11:47:01 -0300

    feat: implementada a função top_5_categories() em ratings.py, melhorada a função top_5_news()

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -4,12 +4,11 @@ from operator import itemgetter
 
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
     query = {"comments_count": {"$gt": 0}}
     news_top_5 = []
     result = search_news(query)
-    new_result = sorted(
-        result, key=itemgetter("comments_count", "title"), reverse=True
+    new_result = multisort(
+        result, (("comments_count", True), ("title", False))
     )
 
     if len(new_result) > 5:
@@ -29,10 +28,57 @@ def top_5_news():
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+    query = {}
+
+    categories = {}
+    categories_top_5 = []
+    result = search_news(query)
+
+    for category in result:
+        if category["category"] not in categories:
+            categories[category["category"]] = 0
+        categories[category["category"]] += 1
+
+    for chave in categories.keys():
+        categories_top_5.append(
+            {"category": chave, "qtde": categories[chave]}
+        )
+
+    order_list = multisort(
+        categories_top_5, (("qtde", True), ("category", False))
+    )
+
+    list_categories_top_5 = qtd_top_categories(order_list)
+
+    return list_categories_top_5
+
+
+# solução encontrada em: https://docs.python.org/pt-br/dev/howto/sorting.html
+# xs -> lista que se quer ordenar
+# specs -> especificações - campo e ordem
+def multisort(xs, specs):
+    for key, reverse in reversed(specs):
+        xs.sort(key=itemgetter(key), reverse=reverse)
+    return xs
+
+
+def qtd_top_categories(data):
+    list_categories_top_5 = []
+    count = 0
+    for category in data:
+        if count < 5:
+            list_categories_top_5.append((category["category"]))
+        count += 1
+
+    return list_categories_top_5
 
 
 if __name__ == "__main__":
     # top_5 = top_5_news()
     # for new in top_5:
     #     print(new)
+    # result = top_5_categories()
+    # print(result)
+    # for category in result:
+    #     print(category)
     pass
